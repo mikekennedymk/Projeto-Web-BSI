163342aee66f16f49f834f0a4b0fe6b19fa87bb3
mikekennedymk <mike.ifpe@gmail.com>
2021-07-13 21:12:07 -0300

    Dinamismo com JavaScript

---
+++ assets/js/livro.js
@@ -0,0 +1,51 @@
+function VisualizarOcultarUm() {
+    var visivel  = $('#divUm').is(':visible');
+    if (visivel){$("#divUm").hide();} 
+    else{
+        $("#divUm").show();
+    }    
+    }
+
+function VisualizarOcultarDois() {
+    var visivel  = $('#divDois').is(':visible');
+    if (visivel){$("#divDois").hide();} 
+    else{
+        $("#divDois").show();
+    }    
+    }
+    function VisualizarOcultarTres() {
+    var visivel  = $('#divTres').is(':visible');
+    if (visivel){$("#divTres").hide();} 
+    else{
+        $("#divTres").show();
+    }    
+    }
+    function VisualizarOcultarQuatro() {
+    var visivel  = $('#divQuatro').is(':visible');
+    if (visivel){$("#divQuatro").hide();} 
+    else{
+        $("#divQuatro").show();
+    }    
+    }
+    function VisualizarOcultarCinco() {
+    var visivel  = $('#divCinco').is(':visible');
+    if (visivel){$("#divCinco").hide();} 
+    else{
+        $("#divCinco").show();
+    }    
+    }
+    function VisualizarOcultarSeis() {
+    var visivel  = $('#divSeis').is(':visible');
+    if (visivel){$("#divSeis").hide();} 
+    else{
+        $("#divSeis").show();
+    }    
+    }
+    function VisualizarOcultarSete() {
+    var visivel  = $('#divSete').is(':visible');
+    if (visivel){$("#divSete").hide();} 
+    else{
+        $("#divSete").show();
+    }    
+    }
+ 
